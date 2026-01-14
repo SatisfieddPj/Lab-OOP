8c2021c17752a21a503dc376ef860920df95eed3
Porjai <126378749+SatisfieddPj@users.noreply.github.com>
2026-01-14 22:54:01 +0700

    Add Lab7_OOP project files and update .gitignore
    Added initial Lab7_OOP NetBeans project files, including build scripts, project configuration, and Java source files for Account, CheckingAccount, Customer, and the main application. Updated .gitignore to exclude Lab7_OOP build, dist, and private directories.

---
+++ Lab7_OOP/src/lab7_oop/CheckingAccount.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab7_oop;
+
+/**
+ *
+ * @author noopo
+ */
+public class CheckingAccount extends Account{
+    private double credit;
+
+    public CheckingAccount() {
+        super(0.0, "");
+        this.credit = 0;
+    }
+    public CheckingAccount(double balance, String name, double credit) {
+        super(balance, name);
+        this.credit = credit;
+    }
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+    public double getCredit() {
+        return credit;
+    }
+    @Override
+    public void withdraw(double a) {
+        if (a > 0) {
+            if (balance - a > 0) {
+                balance -= a;
+                System.out.println(a + " baht is withdrawn from " + name + " and your credit balance is " + credit + ".");
+            } else if ( balance - a < 0 ) {
+                if ( ((balance + credit) - a ) < 0 ) {
+                    System.out.println("Not enough money!");
+                } else {
+                    a -= balance;
+                    credit -= a;
+                    System.out.println(a + " baht is withdrawn from " + name + " and your credit balance is " + credit + ".");
+                }
+            }
+        } else {
+            System.out.println("Input number must be a positive integer.");
+        }
+    }
+    public void withdraw(String a) {
+        double number = Double.parseDouble(a);
+        withdraw(number);
+    }
+ 
+    @Override
+    public String toString() {
+        return ("The " + name + " account has " + balance + " baht and " + credit + " credits.");
+    }
+}
